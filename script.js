8528c9cb492b6c6f616414e7c886d854db8f2fab
Rajudas575 <rajudas575@gmail.com>
2024-11-25 01:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	const firstName = document.getElementById('first-name').value;
+    const lastName = document.getElementById('last-name').value;
+	const fullName = firstName + ' ' + lastName;
 
+    // Display the full name in an alert
+    alert(fullName);
 }
